f15b43efa2673c793e13236d8c5c64980d807d7a
Carmen <2942173241@qq.com>
2025-11-25 10:33:37 +0800

    fix: @Data能让configuration-processor生成元数据

--- sparrow-client/src/main/java/com/example/sparrow/client/config/SparrowConfigProperties.java
+++ sparrow-client/src/main/java/com/example/sparrow/client/config/SparrowConfigProperties.java
@@ -1,7 +1,9 @@
 package com.example.sparrow.client.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Data
 @ConfigurationProperties("sparrow")
 public class SparrowConfigProperties {
     private Boolean enable;
--- sparrow-demo/src/main/java/com/example/sparrow/demo/TestController.java
+++ sparrow-demo/src/main/java/com/example/sparrow/demo/TestController.java
@@ -1,6 +1,8 @@
 package com.example.sparrow.demo;
 
+import com.example.sparrow.client.config.SparrowConfigProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @Value("${first.item:default}")
     private String firstConfig;
+    @Autowired
+    private SparrowConfigProperties sparrowConfigProperties;
 
     @GetMapping
     public String test() {
       log.info("The firstConfig is {}", firstConfig);
+      log.info("The sparrow config properties is {}", sparrowConfigProperties);
       return firstConfig;
     }
 }
