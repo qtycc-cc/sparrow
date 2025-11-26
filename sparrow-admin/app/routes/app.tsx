02c86c8d71e9f3b5a870feae012cc601b127dd98
Carmen <2942173241@qq.com>
2025-11-26 11:15:58 +0800

    refatcor: app form路由重构

--- sparrow-admin/app/routes/app.tsx
+++ sparrow-admin/app/routes/app.tsx
@@ -7,13 +7,8 @@ import { Button } from "~/components/ui/button";
 import { DataTablePagination } from "~/components/data-table-pagination";
 import { Separator } from "~/components/ui/separator";
 import { Grid2x2Check, MoreHorizontal, RefreshCcwIcon } from "lucide-react";
-import { Form, Link, useSearchParams } from "react-router";
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "~/components/ui/alert-dialog";
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "~/components/ui/dialog";
+import { Form, Link, Outlet, useSearchParams, useSubmit } from "react-router";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "~/components/ui/dropdown-menu";
-import { Label } from "~/components/ui/label";
-import { Input } from "~/components/ui/input";
-import { useState } from "react";
 import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "~/components/ui/empty";
 import { toast } from "sonner";
 import { Loader } from "~/components/loader";
@@ -53,7 +48,7 @@ const columns: ColumnDef<App>[] = [
     "header": "操作",
     cell: ({ row }) => {
       const app = row.original;
-      const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+      const submit = useSubmit();
       return (
         <>
           <DropdownMenu>
@@ -67,30 +62,14 @@ const columns: ColumnDef<App>[] = [
               <DropdownMenuItem>
                 <Link className="w-full" to={`/app/${app.id}/config`}>详情</Link>
               </DropdownMenuItem>
-              <DropdownMenuItem variant="destructive" onSelect={() => setShowDeleteDialog(true)}>
+              <DropdownMenuItem variant="destructive" onSelect={() => submit(
+                {}, {
+                  action: `action/delete-app/${app.id}`
+                })}>
                 删除
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-          <AlertDialog open={showDeleteDialog} onOpenChange={setShowDeleteDialog}>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>确认删除？</AlertDialogTitle>
-                <AlertDialogDescription>
-                  此操作无法撤销。这将永久删除该应用及其所有配置和发布。
-                </AlertDialogDescription>
-              </AlertDialogHeader>
-              <AlertDialogFooter>
-                <AlertDialogCancel>取消</AlertDialogCancel>
-                <Form method="delete">
-                  <input type="hidden" name="id" value={app.id} />
-                  <AlertDialogAction type="submit" name="action" value="delete">
-                    确认删除
-                  </AlertDialogAction>
-                </Form>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
         </>
       );
     },
@@ -118,45 +97,6 @@ export async function clientLoader({
   }
   const data = await response.json() as PageResponse<App>;
   return data;
-}
-
-export async function clientAction({
-  request,
-}: Route.ClientActionArgs) {
-  const formData = await request.formData();
-  const { action, ...value } = Object.fromEntries(formData);
-  if (action === "delete") {
-    const response = await fetch(`${import.meta.env.VITE_SPARROW_HOST}/admin/app/${value.id}`, {
-      method: "DELETE"
-    });
-    if (!response.ok) {
-      const problemDetail = await response.json() as ProblemDetail;
-      toast.error("删除失败", {
-        description: problemDetail?.detail
-      });
-      return "Delete failed";
-    }
-    return "Delete success";
-  }
-  if (action === "post") {
-    const response = await fetch(`${import.meta.env.VITE_SPARROW_HOST}/admin/app`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: value.name,
-      }),
-    });
-    if (!response.ok) {
-      const problemDetail = await response.json() as ProblemDetail;
-      toast.error("创建失败", {
-        description: problemDetail?.detail
-      });
-      return "Create failed";
-    }
-    return "Create success";
-  }
 }
 
 export function HydrateFallback() {
@@ -193,43 +133,14 @@ export default function App({
     onPaginationChange: handlePaginationChange
   });
 
-  const [showCreateDialog, setShowCreateDialog] = useState(false);
-
   return (
     <div className="container mx-auto py-5">
+      <Outlet />
       <div className="container mx-auto flex flex-row justify-between items-center mb-4">
         <h1 className="mb-4 text-2xl font-bold">应用列表</h1>
-        <Dialog open={showCreateDialog} onOpenChange={setShowCreateDialog}>
-          <DialogTrigger asChild>
-            <Button
-              onSelect={(e) => e.preventDefault() /**prevent close the dialog */}
-            >
-              新增
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <Form method="post" onSubmit={() => setShowCreateDialog(false)}>
-              <DialogHeader>
-                <DialogTitle>新增应用</DialogTitle>
-              </DialogHeader>
-              <DialogDescription className="mb-4 mt-4 text-1xl">
-                新增应用
-              </DialogDescription>
-              <div className="grid gap-4">
-                <div className="grid gap-3">
-                  <Label htmlFor="appName">name</Label>
-                  <Input required id="appName" name="name" />
-                </div>
-              </div>
-              <DialogFooter className="mt-4">
-                <DialogClose asChild>
-                  <Button variant="outline">取消</Button>
-                </DialogClose>
-                <Button type="submit" name="action" value="post">保存</Button>
-              </DialogFooter>
-            </Form>
-          </DialogContent>
-        </Dialog>
+        <Form action="action/create-app">
+          <Button type="submit">新增应用</Button>
+        </Form>
       </div>
       {appPage.content.length ? (
         <>
