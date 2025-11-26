22803a145ab8a715683fd393745e37f693c2bc99
Carmen <2942173241@qq.com>
2025-11-26 10:49:30 +0800

    feat: loader添加渐进

--- sparrow-admin/app/components/loader.tsx
+++ sparrow-admin/app/components/loader.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "./ui/spinner";
 
 export function Loader() {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 dark:bg-gray-950/70" >
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 dark:bg-gray-950/70 animate-in fade-in fade-out duration-500">
       <Spinner className="size-12" />
     </div>
   );
