7350f878231c3ea55314370888f9881b38b47cd2
Carmen <2942173241@qq.com>
2025-12-11 20:02:05 +0800

    feat: 添加异常日志

--- sparrow-configservice/src/main/java/com/example/sparrow/configservice/exception/GlobalExceptionHandler.java
+++ sparrow-configservice/src/main/java/com/example/sparrow/configservice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.sparrow.configservice.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,12 +14,14 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  * Custom exception can extend {@link org.springframework.web.ErrorResponseException}
  * then spring can resolve without add handler in this class
  */
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(IllegalStateException.class)
     public ProblemDetail handleIllegalStateException(IllegalStateException ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
         problemDetail.setTitle("Illegal state");
+        logException(ex);
         return problemDetail;
     }
 
@@ -26,6 +29,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ProblemDetail handleThrowable(Throwable ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
         problemDetail.setTitle("Internal server error");
+        logException(ex);
         return problemDetail;
+    }
+
+    private void logException(Throwable ex) {
+        log.error("Something went wrong with throwable ", ex);
     }
 }
